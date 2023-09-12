1441c8804dfd5332c34a3abe3ad3cb222eb32754
Muhammad Bilal <bilalshahidvayani@gmail.com>
2023-09-12 15:14:06 +0500

    task

---
+++ app1.js
@@ -0,0 +1,150 @@
+// // Chapter 6 (Math Expression II)
+// // q1
+// var num =(2++)
+// console.log(num);
+// // q2
+// var num1 =(num--)
+// console.log=num1;
+// // q3
+// var x = 50;
+// var y = x++;
+// console.log=y;
+// // q4
+// var y = 50;
+// var z = --y;
+// console.log=z;
+// // q5
+// var newnumber=--num;
+// // q6
+// var newnumber=++num;
+// // q7
+// var num = 5;
+// num++;
+// alert=num;
+// Chapter 7 (Math Expression III)
+// q1
+var Num = 2 + (2 * 6);
+console.log=num;
+// q2
+var calculatedNum = (2 + 2) * 6;
+console.log=calculatedNum;
+// q3
+var calculatedNum = (2 + 2) * (4 + 2);
+console.log = calculatedNum;
+// q4
+var calculatedNum = ((2 + 2) * 4) + 2;
+console.log = calculatedNum;
+// q5
+var cost = (2 + 2) * (4 + 10);
+console.log = cost
+// q6
+var units = (2 + 2) * (4 + 10);
+console.log = units
+// q7
+var pressure = (4 / 2) * 4;
+console.log = pressure
+// Chapter 8 (Concatenating Text Strings)
+// q1
+var num = "2" + "2";
+console.log(num);
+// q2
+var message = "Hello," + "Dolly";
+alert(message);
+// q3
+alert("33" + 3);
+// q4
+var part1 = "Pakistan ";
+var part2 = "Zindabad";
+var message = part1 + part2;
+alert(message);
+// q5
+var stringVariable = "Hello, ";
+var number = 42;
+var concatenatedValue = stringVariable + number;
+// q6
+var firstString = "Hello, ";
+var secondString = "world!";
+var concatenatedResult = firstString + secondString;
+// Chapter 9 (Prompts)
+// q1
+var firstName = prompt("Enter first name");
+// q2
+var country = prompt("Country?", "China");
+// q3
+var yourName = prompt("Enter Your Name");
+// q4
+var userInput = prompt("Please enter a value:", "Default Value");
+// q5
+var firstString = "Hello";
+var secondString = "World";
+var userResponse = prompt(firstString, secondString);
+// q6
+var message = "Please enter your name:";
+var defaultResponse = "John Doe";
+var userResponse = prompt(message, defaultResponse);
+
+alert("You entered: " + userResponse);
+// Chapter 10 (if statments)
+// q1
+var city = "Karachi";
+if (city === "Karachi") {
+    console.log("The City OF Lights");
+}
+// q2
+if (x === y) {
+    var z = prompt("Enter the value of z?");
+}
+// q3
+var zipCode = "10010";
+if (zipCode === "10010") {
+    alert("Karachi");
+} else {
+    alert("Please write the correct city");
+}
+// q4
+var x = 1;
+if (x === 1) {
+    x = 2; 
+}
+// Chapter 11 (Comparison Operators)
+// q1
+if (variable1 !== variable2) {
+  }
+// q2
+if (variable1 >= variable2) {
+  }
+  // q3
+  if (variable !== 42) {
+  variable = 42;
+}
+// q4
+if (number1 !== number2) {
+    alert("Congratulations!");
+  }
+  // q5
+  var firstName = prompt("Enter your first name");
+  if (firstName !== "John") {
+    alert("No match");
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
